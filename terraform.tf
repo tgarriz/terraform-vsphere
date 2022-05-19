b117885490bfcabc04eccd6d2dea1a90b94563a8
tgarriz <tgarriz@gmail.com>
2022-05-18 22:44:17 -0300

    mock

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 variable "proveedor" {
-    description = "Parametros para validar en el vcenter"
-    type = map(string)
-    default = {
-        user = ""
-        pass = ""
-        server = "vcenter508.arba.gov.ar"
-    }
+  description = "Parametros para validar en el vcenter"
+  type        = map(string)
+  default = {
+    user   = "pepe"
+    pass   = "pepe"
+    server = "vcenter508.arba.gov.ar"
+  }
 }
